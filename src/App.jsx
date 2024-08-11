834edc5434e3bfd66d8960c577dd35ea2c0ed874
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2024-08-11 18:54:51 +0100

    install prop-types library to type check the props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [heading, setHeading] = useState("All Might");
+
+  const handleClick = () => {
+    setHeading("Endavor");
+  };
   return (
     <>
-      <h1>React template</h1>
+      <h1>{heading}</h1>
+      <button onClick={handleClick}>change hero</button>
     </>
   );
 }

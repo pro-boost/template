259edc8b263e6f50c3c7f320d651834739596024
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2024-08-22 13:03:49 +0100

    Install React Router package

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,5 @@
-import { useState } from "react";
-import "./App.css";
-
-function App() {
-  const [heading, setHeading] = useState("All Might");
-
-  const handleClick = () => {
-    setHeading("Endavor");
-  };
-  return (
-    <>
-      <h1>{heading}</h1>
-      <button onClick={handleClick}>change hero</button>
-    </>
-  );
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
